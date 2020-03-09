cd7ad14da4ec071d364b4c0dac524a7fac0ac967
adrianavarrete <adri.navarrete.09@gmail.com>
2020-03-09 22:59:42 +0100

    ionic version

--- index.js
+++ index.js
@@ -8,34 +8,32 @@ const ___dirname = path.resolve();
 
 // settings
 app.set('port', process.env.PORT || 8000);
-app.set('json spaces',2);
+app.set('json spaces', 2);
 
 // middleware
 app.use(morgan('dev'));
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
 // routes
 
 // starting the server
 app.listen(app.get('port'), () => {
-    console.log(`Server on port ${app.get('port')}`);
+  console.log(`Server on port ${app.get('port')}`);
 });
 
 app.get('/test', (req, res) => {
-    res.sendFile(path.join(___dirname + '/test.json'));
+  res.sendFile(path.join(___dirname + '/test.json'));
 });
 
 app.post("/hola", (req, res) => {
 
-    console.log("entra");
-  
-    const cosas = {
-      hello: "buenos dias cliente"
-  };
+  const cosas = {
+    respuestaServidor: "Hola, gracias por tu mensaje. Te confirmo que he recibido el siguiente texto --> " + req.body.mensaje
+  }
   console.log(req.body);
-    res.status(200).json(cosas);
-  });
+  res.status(200).send(cosas);
+});
 
 
