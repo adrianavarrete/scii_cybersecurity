d357b9c5052d66890d34848a6af9d4347002444b
adrianavarrete <adri.navarrete.09@gmail.com>
2020-03-17 22:38:11 +0100

    RSA totalmente funcional

--- index.js
+++ index.js
@@ -3,8 +3,22 @@ const app = express();
 const morgan = require('morgan');
 const cors = require('cors');
 const path = require('path');
+const rsa = require('rsa');
+const bigconv = require('bigint-conversion');
 
 const ___dirname = path.resolve();
+
+global.puKey;
+global.prKey;
+
+async function claves() {
+  const { publicKey, privateKey } = await rsa.generateRandomKeys(3072);
+
+  puKey = publicKey;
+  prKey = privateKey;
+
+};
+
 
 // settings
 app.set('port', process.env.PORT || 8000);
@@ -20,6 +34,7 @@ app.use(cors());
 
 // starting the server
 app.listen(app.get('port'), () => {
+  claves();
   console.log(`Server on port ${app.get('port')}`);
 });
 
@@ -27,13 +42,36 @@ app.get('/test', (req, res) => {
   res.sendFile(path.join(___dirname + '/test.json'));
 });
 
+app.get('/key', (req, res) => {
+
+  class PublicKey {
+    constructor(e, n) {
+      this.e = bigconv.bigintToHex(e);
+      this.n = bigconv.bigintToHex(n);
+    }
+  }
+
+  publicKey = new PublicKey(
+    puKey.e,
+    puKey.n
+  )
+
+  res.status(200).send(publicKey);
+
+});
+
 app.post("/hola", (req, res) => {
 
+  mensajeRecibido = bigconv.bigintToText(rsa.decrypt(bigconv.hexToBigint(req.body.mensaje.c), prKey.d, puKey.n));
+  respuesta = "Hola, gracias por tu mensaje. Te confirmo que he recibido el siguiente texto --> " + mensajeRecibido
+  respuestaEncriptada = bigconv.bigintToHex(rsa.encrypt(bigconv.textToBigint(respuesta), bigconv.hexToBigint(req.body.mensaje.e), bigconv.hexToBigint(req.body.mensaje.n)));
+
   const cosas = {
-    respuestaServidor: "Hola, gracias por tu mensaje. Te confirmo que he recibido el siguiente texto --> " + req.body.mensaje
+    respuestaServidor: respuestaEncriptada
   }
-  console.log(req.body);
   res.status(200).send(cosas);
 });
+
+
 
 
